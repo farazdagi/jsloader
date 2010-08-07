680e86fee9761332fb812655e885520477ab0eda
Victor Farazdagi <simple.square@gmail.com>
2010-08-07 00:16:44 -0400

    first tests added

--- loader/loader.js
+++ loader/loader.js
@@ -1,3 +1,28 @@
 var JSLoader = JSLoader || (function () {
+    // cached reference to document
+    var d = document; 
+
+    function _load(uris, callback, args, scope) {
+
+        if (args.constructor === Array) {
+            callback.apply(scope || null, args);
+        } else {
+            callback.call(scope || null, args);
+        }
+    }
+
+    return {
+        /**
+         * Load script(s) unobtrusive/non-blocking manner.
+         *
+         * @param String|Array  uris        URI or array of URIs to be loaded
+         * @param Function      callback    Callback function, triggered after all scripts are loaded
+         * @param Array|Object  args        Argument(s) to be passed to callback
+         * @param Object|null   scope       Callback will be triggered in context of given scope
+         */
+        load: function (uris, callback, args, scope) {
+            _load(uris, callback, args, scope);
+        }
+    };
 }) ();
 
--- tests/loader-tests.js
+++ tests/loader-tests.js
@@ -1,15 +1,62 @@
 $(document).ready(function () {
-    module("Sample Unit Test");
+    var log = function (msg) {
+        if(window.console && window.console.log) {
+            window.console.log(msg);
+        }
+    };
 
-    test("Sample", function () {
-        expect(1);
-        ok(true, "Sample Test Passing..");
-    });
+    var callback1 = function(arg1, arg2) {
+        ok(arg1 === 1, "Argument 1 is passed ok");
+        ok(arg2 === 2, "Argument 2 is passed ok");
+        ok(this.the_ultimate_answer === 42, "Container object is accessible via this");
+    },
+
+    passMe = {x: 1, y: 2},
+
+    callback2 = function(theonlyarg) {
+        alert('x');
+        ok(theonlyarg === passMe, theonlyarg + " = " + passMe);
+    },
+
+    // object used as scope (to be passed into Function.call|apply)
+    scope = {
+        the_ultimate_answer: 42
+    };
 
     module("Get Loader");
 
-    test("Get JSLoader", function () {
-
+    test("JSLoader.loader.load() - argument passing", function () {
+        expect(3);
+        
+        // full arguments test
+        JSLoader.load('http://test.qubr.ru/jsloader/tests/js.php?id=1', callback1, [1, 2], scope);
     });
+
+    test("JSLoader.loader.load() - pass array of URIs", function () {
+        expect(3);
+
+        var uris = [
+            'http://test.qubr.ru/jsloader/tests/js.php?id=1',
+            'http://test.qubr.ru/jsloader/tests/js.php?id=2',
+            'http://test.qubr.ru/jsloader/tests/js.php?id=3'
+        ];
+        
+        // full arguments test
+        JSLoader.load(uris, callback1, [1, 2], scope);
+    });
+
+    test("JSLoader.load() - single object passed", function () {
+            alert('x');
+        expect(1);
+        JSLoader.load(
+            'http://test.qubr.ru/jsloader/tests/js.php?id=1',
+            callback2, passMe
+        );
+    });
+
+    // test global scope
+    // test current scope
+    //
+
 
 });
